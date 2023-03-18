390a5121dbe3cee2eba146a478a4b176a06b69a2
maher9393267111 <89878167+maher9393267111@users.noreply.github.com>
2023-03-18 16:02:49 +0300

    dddd

--- src/pages/listing-map-v3/index.jsx
+++ src/pages/listing-map-v3/index.jsx
@@ -5,8 +5,6 @@ import ListingMapV3 from "../../components/listing-half-map/listing-map-v3";
 const index = () => {
   return (
     <>
-
-    
       <Seo pageTitle="Listing - Map V3" />
       <ListingMapV3 />
     </>
